1ddbef2c9d10cd11464be2205f77e5bc22a7f6c8
Rob Rohan <robrohan@gmail.com>
2012-11-01 10:50:08 -0700

    Removing old code from another project

--- GlueyNotesiPhone/GlueyNotesiPhone.dcproj/project/Common.js
+++
@@ -1,8 +0,0 @@
-var GlueyNotes={};
-
-GlueyNotes.pipe = new Sortie.IO.Pipe().GetInstance();
-GlueyNotes.gateway = new Sortie.IO.Gateway(GlueyNotes.pipe, true);
-
-GlueyNotes.selected_list = "todo";
-GlueyNotes.current_hash = "";
-GlueyNotes.app_offset = "http://glueynotes.com";
